c020922aa1ecceff81174544a3c576b866ea622f
aatreyimehta <91235722+aatreyimehta@users.noreply.github.com>
2023-03-28 12:10:31 -0400

    Update RemovePng.py

--- RemovePng.py
+++ RemovePng.py
@@ -1,7 +1,10 @@
+#The purpose of RemovePng.py file is to remove all the original sized grayscale images with ".png" extension as they are unneccesary and will not be used further. 
+#Note: You should run Resize.py first, then run RemovePng.py on the tactile1 folders
+
 #importing os module
 import os
 #path of the folder
-folder_path = (r'/home/student/Project/.../data/tactile/')
+folder_path = (r'/home/student/Project/.../data/tactile1/')
 #using listdir() method to list the files of the folder
 test = os.listdir(folder_path)
 #taking a loop to remove all the images
