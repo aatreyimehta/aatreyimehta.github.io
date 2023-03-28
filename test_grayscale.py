7e924ac88565ecfc0ff5116710b2d1353e7a3b85
aatreyimehta <91235722+aatreyimehta@users.noreply.github.com>
2023-03-28 11:45:09 -0400

    Update test_grayscale.py

--- test_grayscale.py
+++ test_grayscale.py
@@ -1,3 +1,4 @@
+#importing python packages 
 import os
 import json
 import numpy as np
@@ -10,11 +11,13 @@ from generators.generators import create_gen
 from datasets.datasets import get_dataset
 from util import mkdir
 
+#Opt takes a dictionary as input and converts its key-value pairs to object attributes
 class Opt:
      def __init__(self, dictionary):
         for k, v in dictionary.items():
              setattr(self, k, v)
 
+#load_opt  loads a JSON file from the specified path and returns an Opt instance
 def load_opt(path):
     with open(path) as json_file:
         opt = json.load(json_file)
@@ -22,6 +25,7 @@ def load_opt(path):
     opt = Opt(opt)
     return opt
 
+#load_model loads a generator model from the specified path and returns it
 def load_model(model_path,opt,device):
     G = create_gen(opt.gen,opt.input_dim,opt.output_dim,opt.gen_filters,opt.norm,multigpu=False)
     G.to(device)
@@ -35,6 +39,7 @@ def load_data(photo_path,opt):
     dataset = DataLoader(dataset=data, batch_size=1, shuffle=False,num_workers=4)
     return dataset
 
+#unnormalization of source image
 def unnormalize(a):
     return a/2 +0.5
 
@@ -43,22 +48,23 @@ def concat_images(photo,sketch,output):
 
     return torch.cat((photo,sketch,output),2)
 
+
 def save_images(dataset,path):
     for i, batch in enumerate(dataset):
         real_A, real_B = batch[0], batch[1]
         with torch.no_grad():
             out = Gen(real_A.to(device))[0].cpu()
             
-        a = unnormalize(real_A[0])
-        b = unnormalize(real_B[0])
-        out = unnormalize(out)
+        a = unnormalize(real_A[0])   #unnormalized source image
+        b = unnormalize(real_B[0])   #unnormalized target image
+        out = unnormalize(out)      #unnormalized output image
         
         
-        file_name = str(i+1) +".png"
-        save_image(concat_images(a,b,out), os.path.join(path,file_name))
+        file_name = str(i+1) +".png"   #saving the concatenated output as an image
+        save_image(concat_images(a,b,out), os.path.join(path,file_name))   #horizontally concatenated source, target and trained output images
         print(f"file saved at: {os.path.join(path,file_name)}")
 
-opt_path = os.path.join(os.getcwd(),"models","pix2pix","params.txt")
+opt_path = os.path.join(os.getcwd(),"models","pix2pix","params.txt")   #output folder 
 opt = load_opt(opt_path)
 device = torch.device("cuda:0")
 
