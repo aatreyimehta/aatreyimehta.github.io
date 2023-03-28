237e245d67f640d9839b7d3232b9b0509c5998f9
aatreyimehta <91235722+aatreyimehta@users.noreply.github.com>
2023-03-28 01:09:23 -0400

    Update bezier_generator.py

--- bezier_generator.py
+++ bezier_generator.py
@@ -1,11 +1,12 @@
+#importing python packages to begin with
 import random
 import numpy as np
 from math import comb
 
-
+#given a set of control points P, the function will generate point on curve at a given value of parameter x
 def generate_bezier(x, P):
-    n = P.shape[0]
-    b = 0
+    n = P.shape[0]   #n is equal to number of control points
+    b = 0   #variable b is initialized to 0
 
     for i in range(n):
         a = comb(n,i)*((1-x)**(n-i))*(x**i)
@@ -19,7 +20,7 @@ def generate_bezier(x, P):
 if __name__ == "__main__":
     P_1D = 0.25
     CROSS = []
-    NUM_SAMPLES = 50000
+    NUM_SAMPLES = 50000   #number of samples to generate
     for i in range(NUM_SAMPLES):
         x = np.linspace(0, 1, 10000).reshape(-1,1)
         P = np.array([[random.randint(-20,20), random.randint(-20,20)] for i in range(random.randint(2,20))])
@@ -28,4 +29,4 @@ if __name__ == "__main__":
             P = P.reshape(-1,1).flatten()[::2]
         b = generate_bezier(x, P)
         print(f"{i+1}.npy generated")
-        np.save(f"./points/{i+1}.npy", b)
+        np.save(f"./points/{i+1}.npy", b)   #saves b as a numpy array in a file named "{i+1}.npy"
